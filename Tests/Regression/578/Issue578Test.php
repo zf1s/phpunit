c8739682dd2baf35e7fe66ac2f8df60efaf80802
Jack Worman <jworman@healthcall.com>
2022-01-18 20:49:57 -0600

    Made Php 8.1 Compatiable
    Co-authored-by: Elan Ruusamäe <glen@pld-linux.org>

--- Tests/Regression/578/Issue578Test.php
+++ Tests/Regression/578/Issue578Test.php
@@ -3,21 +3,21 @@ class Issue578Test extends PHPUnit_Framework_TestCase
 {
     public function testNoticesDoublePrintStackTrace()
     {
-        $this->iniSet('error_reporting', E_ALL | E_NOTICE);
-        if (PHP_VERSION_ID < 80000) {
+        try {
+            $this->iniSet('error_reporting', E_ALL | E_NOTICE);
             trigger_error('Stack Trace Test Notice', E_NOTICE);
-        } else {
-            trigger_error('Invalid error type specified', E_USER_NOTICE);
+        } catch (\ValueError $e) {
+            \trigger_error('Invalid error type specified');
         }
     }
 
     public function testWarningsDoublePrintStackTrace()
     {
-        $this->iniSet('error_reporting', E_ALL | E_NOTICE);
-        if (PHP_VERSION_ID < 80000) {
+        try {
+            $this->iniSet('error_reporting', E_ALL | E_NOTICE);
             trigger_error('Stack Trace Test Notice', E_WARNING);
-        } else {
-            trigger_error('Invalid error type specified', E_USER_WARNING);
+        } catch (\ValueError $e) {
+            \trigger_error('Invalid error type specified');
         }
     }
 
