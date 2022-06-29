7744ecebeadce72dae0b2b1c8364e5621a7d9867
Michal Kruczek <partikus@users.noreply.github.com>
2022-06-29 13:55:01 +0200

    php 8.x compat - trigger_error

--- Tests/Regression/578/Issue578Test.php
+++ Tests/Regression/578/Issue578Test.php
@@ -4,13 +4,21 @@ class Issue578Test extends PHPUnit_Framework_TestCase
     public function testNoticesDoublePrintStackTrace()
     {
         $this->iniSet('error_reporting', E_ALL | E_NOTICE);
-        trigger_error('Stack Trace Test Notice', E_NOTICE);
+        if (PHP_VERSION_ID < 80000) {
+            trigger_error('Stack Trace Test Notice', E_NOTICE);
+        } else {
+            trigger_error('Invalid error type specified', E_USER_NOTICE);
+        }
     }
 
     public function testWarningsDoublePrintStackTrace()
     {
         $this->iniSet('error_reporting', E_ALL | E_NOTICE);
-        trigger_error('Stack Trace Test Notice', E_WARNING);
+        if (PHP_VERSION_ID < 80000) {
+            trigger_error('Stack Trace Test Notice', E_WARNING);
+        } else {
+            trigger_error('Invalid error type specified', E_USER_WARNING);
+        }
     }
 
     public function testUnexpectedExceptionsPrintsCorrectly()
