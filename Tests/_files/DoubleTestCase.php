bdecae68cadbafb0b65e5dada90db21daa51431f
Maciej Holyszko <14310995+falkenhawk@users.noreply.github.com>
2026-04-02 08:17:31 +0200

    add missing type guard to DoubleTestCase::run()

--- Tests/_files/DoubleTestCase.php
+++ Tests/_files/DoubleTestCase.php
@@ -16,6 +16,10 @@ class DoubleTestCase implements PHPUnit_Framework_Test
 
     public function run($result = NULL)
     {
+        if ($result !== NULL && !$result instanceof PHPUnit_Framework_TestResult) {
+            throw PHPUnit_Util_InvalidArgumentHelper::factory(1, 'PHPUnit_Framework_TestResult');
+        }
+
         $result->startTest($this);
 
         $this->testCase->runBare();
